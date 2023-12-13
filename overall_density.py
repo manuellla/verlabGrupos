5ecf3012a6baa370222757048303b3fbfcc75fc5
manuela <82252712+manuellla@users.noreply.github.com>
2023-12-13 13:24:44 -0300

    Atualizando

---
+++ overall_density.py
@@ -0,0 +1,261 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import itertools
+import pickle
+
+from sklearn.neighbors import KDTree
+from sklearn.cluster import DBSCAN
+from scipy.signal import medfilt
+from person import Person
+from matplotlib import cm
+from math import *
+
+class OverallDensity(object):
+    #
+    temporal_weight = dict()
+    dict = {'Intimate': 0.45, 'Personal': 1.2, 'Social': 3.6, 'Public': 10.0}
+
+    """ Public Methods """
+
+    def __init__(self, person=None, zone=None, map_resolution=100, window_size=1):
+        #
+        self.x = np.empty([0, 0])
+        self.y = np.empty([0, 0])
+        self.G = nx.Graph()
+        self.cluster = list()
+        self.density = list()
+        self.max_dist = list()
+        self.person = person
+        self.window_size = window_size
+        self.proxemics_th = self.dict[zone]
+        self.map_resolution = map_resolution
+
+    def set_zone(self, value=None):
+        self.dict['Personal'] = value
+        self.proxemics_th = value
+
+    def norm_range(self, value, in_max, in_min, out_max, out_min):
+        return ((out_max - out_min) * (value - in_min)) / (in_max - in_min) + out_min;
+
+    def pose2map(self, pose):
+        return int(self.norm_range(pose, max(self.x), min(self.x), len(self.x) - 1, 0))
+
+    def map2pose(self, pose):
+        return float(self.norm_range(pose, len(self.x) - 1, 0, max(self.x), min(self.x)))
+
+    def get_nearest_neighbors(self, seed, dataset):
+        tree = KDTree(dataset, leaf_size=1)
+        result, dists = tree.query_radius([seed], r=self.proxemics_th ** 2.0, return_distance=True, sort_results=True, count_only=False)
+        result = result.item()
+        dists = dists.item()
+        temp_count_only = dists <= self.proxemics_th
+        result = result[temp_count_only]
+        dists = dists[temp_count_only]
+        self.max_dist.append(max(dists))
+        return result
+
+    def local_density(self, p_i, p):
+        sigma_h = 2.0
+        sigma_r = 1.0
+        sigma_s = 4.0 / 3.0
+        alpha = np.arctan2(p.y - p_i.y, p.x - p_i.x) - p_i.th - pi / 2.0
+        nalpha = np.arctan2(np.sin(alpha), np.cos(alpha))  # Normalizando no intervalo [-pi, pi)
+        sigma = sigma_r if nalpha <= 0 else sigma_h
+        a = cos(p_i.th) ** 2.0 / 2.0 * sigma ** 2.0 + sin(p_i.th) ** 2.0 / 2.0 * sigma_s ** 2.0
+        b = sin(2.0 * p_i.th) / 4.0 * sigma ** 2.0 - sin(2.0 * p_i.th) / 4.0 * sigma_s ** 2.0
+        c = sin(p_i.th) ** 2.0 / 2.0 * sigma ** 2.0 + cos(p_i.th) ** 2.0 / 2.0 * sigma_s ** 2.0
+        rad = (fabs(np.arctan2(p.y - p_i.y, p.x - p_i.x)) - fabs(p_i.th))
+        AGF = np.exp(-(a * (p.x - p_i.x) ** 2.0 + 2.0 * b * (p.x - p_i.x) * (p.y - p_i.y) + c * (p.y - p_i.y) ** 2.0))
+        return AGF
+
+    def is_edge(self, z, x, y):
+        #
+        if z[x, y] != 0:
+            if x > 0 and x < z.shape[0] - 1 and y > 0 and y < z.shape[1] - 1:
+                if bool(z[x - 1, y] <= z[x, y]) ^ bool(z[x + 1, y] <= z[x, y]) or bool(z[x, y - 1] <= z[x, y]) ^ (
+                        z[x, y + 1] <= z[x, y]):
+                    return z[x, y]
+
+    def set_threshold(self, z, x, y, th):
+        #
+        if z[x, y] >= th:
+            if x > 0 and x < z.shape[0] - 1 and y > 0 and y < z.shape[1] - 1:
+                if z[x - 1, y] > th and z[x + 1, y] > th and z[x, y - 1] > th and z[x, y + 1] > th:
+                    return 0.0
+                else:
+                    return 1.0  # Value that defines the density of the point in the plot
+        return 0.0
+
+    def ccw(self, A, B, C):
+        # http://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
+        return fabs(C[1] - A[1]) * fabs(B[0] - A[0]) > fabs(B[1] - A[1]) * fabs(C[0] - A[0])
+
+    def line_intersection(self, A, B, C, D):
+        return self.ccw(A, C, D) != self.ccw(B, C, D) and self.ccw(A, B, C) != self.ccw(A, B, D)
+
+    def edges_weigth(self, pi, pj):
+        pi2pj = sqrt((pi.x - pj.x) ** 2 + (pi.y - pj.y) ** 2)
+        piFoA_x = pi.x + ((pi2pj / 2) * cos(pi.th))
+        piFoA_y = pi.y + ((pi2pj / 2) * sin(pi.th))
+        pjFoA_x = pj.x + ((pi2pj / 2) * cos(pj.th))
+        pjFoA_y = pj.y + ((pi2pj / 2) * sin(pj.th))
+        deltaFoA = sqrt((piFoA_x - pjFoA_x) ** 2 + (piFoA_y - pjFoA_y) ** 2)
+        pi2pjFoA = sqrt((pi.x - pjFoA_x) ** 2 + (pi.y - pjFoA_y) ** 2)
+        pj2piFoA = sqrt((pj.x - piFoA_x) ** 2 + (pj.y - piFoA_y) ** 2)
+        exponent = 1.0
+        if self.line_intersection([fabs(pi.x), fabs(pi.y)], [fabs(pj.x), fabs(pj.y)], [piFoA_x, piFoA_y],
+                                  [pjFoA_x, pjFoA_y]):
+            exponent = 2.0
+        if (deltaFoA) >= min(pi2pjFoA, pj2piFoA):
+            exponent += 0.5
+        if pi2pj != 0:
+            return round((1 - (deltaFoA / (pi2pj * 2.0))) ** exponent, 2)
+        return 0
+
+    def make_graph(self):
+        dataset = np.array([[p.x, p.y] for p in self.person], dtype=float)
+        for seed in dataset:
+            neighbors = self.get_nearest_neighbors(seed, dataset)
+            for node in neighbors:
+                self.G.add_node(self.person[node].id_node, pos=(self.person[node].x, self.person[node].y))
+                if neighbors[0] == node:
+                    continue
+                # self.G.add_node(self.person[node].id_node, pos=(self.person[node].x ,self.person[node].y))
+                level_interaction = self.edges_weigth(self.person[neighbors[0]], self.person[node])
+                key = str(int(self.person[neighbors[0]].id_node)) + '_' + str(int(self.person[node].id_node))
+                self.temporal_weight.setdefault(key, []).append(level_interaction)
+                if self.window_size > len(self.temporal_weight[key]):
+                    for padding in range(1, self.window_size):
+                        self.temporal_weight.setdefault(key, []).append(level_interaction)
+                if self.window_size != 0:
+                    median_filter = medfilt(self.temporal_weight[key], self.window_size)
+                    level_interaction = median_filter[int(self.window_size / -2)]
+                if level_interaction >= 0.5 or self.proxemics_th > self.dict['Personal']:
+                    self.G.add_edge(self.person[neighbors[0]].id_node, self.person[node].id_node,
+                                    weight=level_interaction)
+
+    def boundary_estimate(self):
+        #
+        margin = max(4.0, self.proxemics_th)
+        self.x = np.linspace(min([p.x for p in self.person]) - margin,
+                             max([p.x for p in self.person]) + margin, self.map_resolution)
+        self.y = np.linspace(min([p.y for p in self.person]) - margin,
+                             max([p.y for p in self.person]) + margin, self.map_resolution)
+        X, Y = np.meshgrid(self.x, self.y)
+        self.density = []
+        self.cluster = []
+        #
+        for component in nx.connected_components(self.G):
+            if len(component) > 1:
+                sigma_th = max(self.max_dist)
+                surrounding_th = max(self.proxemics_th, sigma_th)
+            else:
+                sigma_th = self.dict['Social']
+                surrounding_th = min(self.proxemics_th, sigma_th)
+            rows = set()
+            cols = set()
+            for node in component:
+                pose = [[p.x, p.y] for p in self.person if p.id_node == node]
+                pose_x = pose[0][0]
+                pose_y = pose[0][1]
+                pose2map_col = int(self.norm_range(pose_x, max(self.x), min(self.x), len(self.x) - 1, 0))
+                pose2map_row = int(self.norm_range(pose_y, max(self.y), min(self.y), len(self.y) - 1, 0))
+                deltaX = (X[pose2map_row, pose2map_col] - X)
+                deltaY = (Y[pose2map_row, pose2map_col] - Y)
+                euclidean = np.sqrt(deltaX ** 2.0 + deltaY ** 2.0)
+                for row in range(Y.shape[0]):
+                    for col in range(X.shape[0]):
+                        if euclidean[row, col] <= surrounding_th:
+                            rows.add(row)
+                            cols.add(col)
+            #
+            local_cluster = np.zeros(X.shape, dtype=float)
+            local_density = np.zeros(X.shape, dtype=float)
+            for node in component:
+                person = [p for p in self.person if p.id_node == node]
+                for row in rows:
+                    for col in cols:
+                        cell = Person(X[row, col], Y[row, col])
+                        local_density[row, col] += self.local_density(person[0], cell)
+            #
+            if len(set(component)) > 1:
+                threshold = np.ma.masked_equal(local_density, 0).mean()
+            else:
+                threshold = exp(-min(self.proxemics_th, self.dict['Personal']) ** 2.0 / 2.0)
+            #
+            for row in rows:
+                for col in cols:
+                    local_cluster[row, col] = self.set_threshold(local_density, row, col, threshold)
+            self.cluster.append(local_cluster)
+            self.density.append(local_density)
+
+    def draw(self, drawDensity=False, drawCluster=False, drawGraph=False, plot=plt):
+        if (drawCluster):
+            X, Y = np.meshgrid(self.x, self.y)
+            color = np.linspace(0, 1, len(self.cluster))
+            for idx, cluster in enumerate(self.cluster):
+                plot.scatter(X, Y, cluster, edgecolors=plt.cm.Dark2(color[idx]))
+
+        if (drawGraph):
+            pos = nx.get_node_attributes(self.G, 'pos')
+            nx.draw_networkx(self.G, pos, node_size=000, with_labels=False)
+            labels = nx.get_edge_attributes(self.G, 'weight')  # (self.G, 'None')
+            nx.draw_networkx_edge_labels(self.G, pos, edge_labels=labels, font_size=15)
+            # Uncomment the line below to plot the labels
+            # nx.draw_networkx_labels(self.G,pos,font_size=20,font_family='sans-serif')
+        if (drawDensity):
+            fig = plt.figure()
+            ax = fig.add_subplot(projection='3d')
+            X, Y = np.meshgrid(self.x, self.y)
+            matrix = np.zeros((X.shape[0], Y.shape[0]))
+            for density in self.density:
+                matrix[:, :] += density[:, :]
+            Z = np.zeros((X.shape[0], Y.shape[0])) + 0.609730865463
+            ax.plot_surface(X, Y, Z, color='red', linewidth=0)
+            suface = ax.plot_surface(X, Y, matrix, cmap='jet', linewidth=0)
+            plt.colorbar(suface)
+            ax.set_xlabel('x (meters)')
+            ax.set_ylabel('y (meters)')
+            ax.set_zlabel('Z')
+            ax.view_init(elev=3., azim=-45)
+            plt.axis([self.x[0], self.x[len(self.x) - 1], self.y[0], self.y[len(self.y) - 1]])
+        plot.axis('equal')
+
+    def precision_recall(self, f):
+        #
+        est = []
+        graph = list(nx.connected_components(self.G))
+        for component in nx.connected_components(self.G):
+            # remove singleton elements
+            if set(component).isdisjoint(nx.isolates(self.G)):
+                est.append(np.array([list(component)], dtype=np.uint8))
+        return est
+    
+    def recalculate_social_zones(self):
+        """Recalcula as zonas sociais com base nas novas posições."""
+        # Usando DBSCAN para identificar clusters baseados na proximidade
+        coords = np.vstack((self.x, self.y)).T
+        clustering = DBSCAN(eps=self.proxemics_th, min_samples=1).fit(coords)
+        self.labels = clustering.labels_
+
+        # Calculando o centro e o raio de cada cluster
+        self.zones = {}
+        for label in np.unique(self.labels):
+            if label != -1:  # Ignorando outliers
+                indices = np.where(self.labels == label)[0]
+                members = coords[indices]
+                center = np.mean(members, axis=0)
+                radius = np.max(np.linalg.norm(members - center, axis=1)) + 5  # Raio + margem
+                self.zones[label] = {'center': center, 'radius': radius}
+
+    def draw_social_zones(self, ax):
+        """Desenha as zonas sociais no eixo fornecido."""
+        for zone in self.zones.values():
+            circle = plt.Circle(zone['center'], zone['radius'], color='blue', fill=False, linestyle='--')
+            ax.add_patch(circle)
+
+    def update_positions(self, people):
+        """Atualiza as posições com base em uma lista de objetos Person."""
+        self.x = np.array([person.x for person in people])
+        self.y = np.array([person.y for person in people])
